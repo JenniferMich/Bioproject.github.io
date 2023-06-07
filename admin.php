7d28736e7b3b59ff80af9e5a11546dcfc8acf896
Michelle Celedón <131729686+JenniferMich@users.noreply.github.com>
2023-06-06 18:25:30 -0600

    Segunda parte de vista

--- admin.php
+++ admin.php
@@ -1,12 +1,53 @@
+<?php
+require_once("C:/wamp/www/appi4/Controlador/controlador.php");
+  @session_start();
+
+  //si existe la sesion y el usurio es tipo 2 entra 
+  if(isset($_SESSION['usuario']) && $_SESSION['tipo']==1){
+    echo "<script>alert('Hola Admin'); 
+    </script>";
+  //si solo existe la sesion lo redirige la paguina de usuarios 
+  }
+  else if(isset($_SESSION['usuario']) && $_SESSION['tipo']==2){
+    echo "<script>alert('pagina solo para administradores');
+    window.location.href='pagina.php'; 
+    </script>";
+  
+
+    //si no destrulle la sesion
+  }else if(isset($_SESSION['usuario']) && $_SESSION['tipo']==3){
+    echo "<script>alert('pagina solo para administradores');
+    window.location.href='pagina2.php'; 
+    </script>";
+  
+
+    //si no destrulle la sesion
+  }else{
+    session_destroy();
+    echo "<script>alert('no has iniciado sesión ');
+    window.location.href='login.html'; 
+    </script>";
+  }
+  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Sitio Admin</title>
 </head>
 <body>
-    <h1>Administrador</h1>
+    <h1>
+        Hola admin 
+        <?php
+            echo $_SESSION['usuario'];
+            echo $_SESSION['tiempo'];
+            echo $_SESSION['cerrar'];
+        ?>
+    </h1>
+    
 </body>
 </html>
---
+++ pagina.php
@@ -0,0 +1,60 @@
+<?php
+require_once("C:/xampp/htdocs/appi4/Controlador/controlador.php");
+  @session_start();
+
+  //si existe la sesion y el usario es tipo 1 lo manda a la paguina de admin
+  if(isset($_SESSION['usuario']) && $_SESSION['tipo']==1){
+    header("Location:../admin.php");
+  }
+
+  // si solo existe la sesion entra a la paguina que es de usuario
+  else if(isset($_SESSION['usuario']) && $_SESSION['tipo']==2){
+    $nombreU=$_SESSION['usuario'];
+    if((time() - $_SESSION['tiempo']) > 120 ){
+      session_destroy();
+      echo "<script>alert('Tu sesion expiro, inicia nuevamente');
+      window.location.href='../login.html'; 
+      </script>";
+      
+    }else{
+        header("Location:../pagina2.php");
+    }
+    
+  } 
+  else if(isset($_SESSION['usuario']) && $_SESSION['tipo']==3){
+    $nombreU=$_SESSION['usuario'];
+    if((time() - $_SESSION['tiempo']) > 120 ){
+      session_destroy();
+      echo "<script>alert('Tu sesion expiro, inicia nuevamente');
+      window.location.href='../login.html'; 
+      </script>";
+      
+    }else{
+      echo "<script>alert('Bienvenido');
+      </script>"; 
+    }
+    
+  }  
+    
+  else{
+    session_destroy();
+    echo "<script>alert('no has iniciado sesión ');
+    window.location.href='../login.html'; 
+    </script>";
+  }
+ 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hola</h1>
+</body>
+</html>
